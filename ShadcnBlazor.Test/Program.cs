2193606a4fca7e97aad2dd16d4be187851ef0937
ChiaraBm <chiara.bm04@gmail.com>
2025-12-03 16:17:24 +0100

    Added extension method for service registration. Updated installation guide

--- ShadcnBlazor.Test/Program.cs
+++ ShadcnBlazor.Test/Program.cs
@@ -1,17 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ShadcnBlazor.Interop;
+using ShadcnBlazor;
 using ShadcnBlazor.Test.UI;
-using TailwindMerge.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddTailwindMerge();
-
-builder.Services.AddScoped<InteropService>();
-builder.Services.AddScoped<PositionService>();
+builder.Services.AddShadcnBlazor();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ ShadcnBlazor/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using ShadcnBlazor.Interop;
+using TailwindMerge.Extensions;
+
+namespace ShadcnBlazor;
+
+public static class Extensions
+{
+    public static void AddShadcnBlazor(this IServiceCollection collection)
+    {
+        collection.TryAddScoped<InteropService>();
+        collection.TryAddScoped<PositionService>();
+        collection.AddTailwindMerge();
+    }
+}
