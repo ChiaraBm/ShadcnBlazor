35e884287297d0722a7c70e6cac8e90405a542d4
ChiaraBm <chiara.bm04@gmail.com>
2025-12-05 14:47:18 +0100

    Started implementing modular data table component

---
+++ ShadcnBlazor/DataGrids/Types.cs
@@ -0,0 +1,13 @@
+﻿namespace ShadcnBlazor.DataGrids;
+
+public class DataGridRequest<TGridItem>
+{
+    public int StartIndex { get; set; }
+    public int Length { get; set; }
+}
+
+public class DataGridResponse<TGridItem>
+{
+    public TGridItem[] Data { get; set; }
+    public int TotalLength { get; set; }
+}
---
+++ ShadcnBlazor/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace ShadcnBlazor;
+
+internal static class Helper
+{
+    internal static string GetPropertyName<TGridItem>(Expression<Func<TGridItem, object>> field)
+    {
+        MemberExpression memberExpression = null;
+
+        if (field.Body is MemberExpression member)
+        {
+            memberExpression = member;
+        }
+        
+        else if (field.Body is UnaryExpression { NodeType: ExpressionType.Convert } unary)
+        {
+            memberExpression = (unary.Operand as MemberExpression)!;
+        }
+
+        if (memberExpression == null)
+            throw new ArgumentException("Expression must be a member access", nameof(field));
+
+        return memberExpression.Member.Name;
+    }
+}
