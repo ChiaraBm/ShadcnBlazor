de423e057ea726275ade81c18a58941d362cd497
ChiaraBm <chiara.bm04@gmail.com>
2025-12-08 11:01:36 +0100

    Improved data grid. Implemented search, selection and pagination. Added custom rows

--- ShadcnBlazor/DataGrids/Abstractions/ColumnBase.razor.cs
+++ ShadcnBlazor/DataGrids/Abstractions/ColumnBase.razor.cs
@@ -8,13 +8,24 @@ public abstract partial class ColumnBase<TGridItem>
     [CascadingParameter] public DataGrid<TGridItem> Grid { get; set; }
     
     [Parameter] public int Order { get; set; }
+    [Parameter] public string Identifier { get; set; }
+
+    [Parameter] public bool DefaultVisible { get; set; } = true;
+    public bool IsVisible { get; set; }
+    
+    [Parameter] public bool IsFilterable { get; set; }
+    [Parameter] public string? DefaultFilter { get; set; }
+    public string? Filter { get; set; }
 
     protected override void OnInitialized()
     {
+        IsVisible = DefaultVisible;
+        Filter = DefaultFilter;
+        
         Grid.RegisterColumn(this);
     }
     
     public abstract void RenderCell(RenderTreeBuilder __builder, TGridItem item);
 
-    protected virtual Task ResetAsync() => Task.CompletedTask;
+    public virtual Task ResetAsync() => Task.CompletedTask;
 }
--- ShadcnBlazor/DataGrids/Types.cs
+++ ShadcnBlazor/DataGrids/Types.cs
@@ -4,6 +4,8 @@ public class DataGridRequest<TGridItem>
 {
     public int StartIndex { get; set; }
     public int Length { get; set; }
+    public Dictionary<string, string> Filters { get; set; }
+    public string? SearchTerm { get; set; }
 }
 
 public class DataGridResponse<TGridItem>
