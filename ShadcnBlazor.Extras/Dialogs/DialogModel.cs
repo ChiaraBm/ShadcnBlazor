3fe512f55438a0491a5990ff35e6b23b6e59b7fb
ChiaraBm <chiara.bm04@gmail.com>
2025-12-11 08:38:57 +0100

    Implemented dialog service. Fixed wrongly positioned event handler call in base dialog

---
+++ ShadcnBlazor.Extras/Dialogs/DialogModel.cs
@@ -0,0 +1,13 @@
+﻿using ShadcnBlazor.Dialogs;
+
+namespace ShadcnBlazor.Extras.Dialogs;
+
+public class DialogModel
+{
+    public Type ComponentType { get; set; }
+    public Dictionary<string, object?> Attributes { get; set; }
+    public Dialog Dialog { get; set; }
+    public string ClassName { get; set; }
+    public bool CloseOnBackdropClick { get; set; }
+    public bool ShowCloseButton { get; set; } = true;
+}
