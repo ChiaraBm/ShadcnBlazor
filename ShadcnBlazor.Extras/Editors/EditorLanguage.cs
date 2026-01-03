943745fde93e114ccd31abf5fe5cfc3890e0bc64
ChiaraBm <chiara.bm04@gmail.com>
2026-01-03 14:38:34 +0100

    Added CodeMirror interop. Implemented Editor component

---
+++ ShadcnBlazor.Extras/Editors/EditorLanguage.cs
@@ -0,0 +1,14 @@
+namespace ShadcnBlazor.Extras.Editors;
+
+public enum EditorLanguage
+{
+    None = 0,
+    Javascript = 1,
+    Html = 2,
+    Json = 3,
+    Sql = 4,
+    Yaml = 5,
+    Php = 6,
+    Xml = 7,
+    Python = 8
+}
