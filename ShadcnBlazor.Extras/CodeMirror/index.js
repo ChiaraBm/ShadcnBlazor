d55e8c0267c13c7c739d00f5bc8b0d41070d2348
ChiaraBm <chiara.bm04@gmail.com>
2026-01-03 14:38:34 +0100

    Added CodeMirror interop. Implemented Editor component

---
+++ ShadcnBlazor.Extras/CodeMirror/index.js
@@ -0,0 +1,78 @@
+import {EditorView, basicSetup} from "codemirror";
+import {javascript} from "@codemirror/lang-javascript";
+import {python} from "@codemirror/lang-python";
+import {php} from "@codemirror/lang-php";
+import {json} from "@codemirror/lang-json";
+import {yaml} from "@codemirror/lang-yaml";
+import {html} from "@codemirror/lang-html";
+import {sql} from "@codemirror/lang-sql";
+import {xml} from "@codemirror/lang-xml";
+import {githubDark} from "@fsegurai/codemirror-theme-bundle";
+
+const roundedTheme = EditorView.theme({
+    "&": {
+        borderRadius: "8px",
+        overflow: "hidden"
+    },
+    ".cm-gutters": {
+        borderRadius: "8px 0 0 8px"
+    }
+})
+
+function getLanguageExtension(language) {
+    switch (language) {
+        case "javascript":
+            return javascript();
+
+        case "html":
+            return html();
+
+        case "json":
+            return json();
+
+        case "sql":
+            return sql();
+
+        case "yaml":
+            return yaml();
+
+        case "php":
+            return php();
+
+        case "xml":
+            return xml();
+
+        case "python":
+            return python();
+
+        default:
+            return javascript();
+    }
+}
+
+window.CodeMirrorAdapter = {
+    init: function (element, initialValue, dotnetRef, language) {
+        const extensions = [
+            basicSetup,
+            githubDark,
+            roundedTheme
+        ];
+
+        if (language)
+            extensions.push(getLanguageExtension(language));
+
+        return new EditorView({
+            doc: initialValue,
+            extensions: extensions,
+            parent: element
+        });
+    },
+    setValue: function (view, newValue) {
+        view.dispatch({
+            changes: {from: 0, to: view.state.doc.length, insert: newValue}
+        });
+    },
+    getValue: function (view) {
+        return view.state.doc.toString();
+    }
+};
