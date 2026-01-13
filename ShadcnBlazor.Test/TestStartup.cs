72eaa55be5f47bd7f260ef351f474a675e13333d
ChiaraBm <chiara.bm04@gmail.com>
2026-01-13 15:37:55 +0100

    Restructed test projects so the server and wasm project use the same codebase. Converted sln to slnx

---
+++ ShadcnBlazor.Test/TestStartup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShadcnBlazor.Extras;
+
+namespace ShadcnBlazor.Test;
+
+public static class TestStartup
+{
+    public static void AddTest(this IServiceCollection services)
+    {
+        services.AddShadcnBlazor();
+        services.AddShadcnBlazorExtras();
+    }
+}
