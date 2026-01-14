384e373e3bd0f7e7e7422b1bd0578a82469530ae
ChiaraBm <chiara.bm04@gmail.com>
2026-01-14 08:39:16 +0100

    Migrated components to use SectionContent with PortalNames for content rendering and added PortalOutlet implementation.

---
+++ ShadcnBlazor/Portals/PortalNames.cs
@@ -0,0 +1,19 @@
+﻿namespace ShadcnBlazor.Portals;
+
+internal static class PortalNames
+{
+    internal const string AlertDialog = "AlertDialog";
+    internal const string ContextMenu = "ContextMenu";
+    internal const string ContextMenuSub = "ContextMenuSub";
+    internal const string Dialog = "Dialog";
+    internal const string Drawer = "Drawer";
+    internal const string DropdownMenu = "DropdownMenu";
+    internal const string DropdownMenuSub = "DropdownMenuSub";
+    internal const string HoverCard = "HoverCard";
+    internal const string Menubar = "Menubar";
+    internal const string MenubarSub = "MenubarSub";
+    internal const string Popover = "Popover";
+    internal const string Select = "Select";
+    internal const string Sheet = "Sheet";
+    internal const string Tooltip = "Tooltip";
+}
