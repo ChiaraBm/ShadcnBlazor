9ae5dc45eb38c1f43a883a88105f7aa1c5d31f4f
ChiaraBm <chiara.bm04@gmail.com>
2026-01-26 10:06:24 +0100

    Implemented field components. Added demo and showcase page

---
+++ ShadcnBlazor/Fields/FieldLegendVariant.cs
@@ -0,0 +1,7 @@
+﻿namespace ShadcnBlazor.Fields;
+
+public enum FieldLegendVariant
+{
+    Legend = 0,
+    Label = 1
+}
---
+++ ShadcnBlazor/Fields/FieldOrientation.cs
@@ -0,0 +1,8 @@
+﻿namespace ShadcnBlazor.Fields;
+
+public enum FieldOrientation
+{
+    Vertical = 0,
+    Horizontal = 1,
+    Responsive = 2
+}
