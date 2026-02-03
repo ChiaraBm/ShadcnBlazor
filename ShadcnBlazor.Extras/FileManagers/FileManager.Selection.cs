38b888221828f13b20b54876afb8c994996e2b34
ChiaraBm <chiara.bm04@gmail.com>
2026-02-03 16:42:06 +0100

    Started implementing selection for file manager

---
+++ ShadcnBlazor.Extras/FileManagers/FileManager.Selection.cs
@@ -0,0 +1,26 @@
+﻿namespace ShadcnBlazor.Extras.FileManagers;
+
+public partial class FileManager
+{
+    private readonly List<FsEntry> SelectedEntries = new();
+
+    private async Task ToggleAllAsync(bool toggle)
+    {
+        SelectedEntries.Clear();
+        
+        if(toggle)
+            SelectedEntries.AddRange(CurrentEntries);
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async Task ToggleAsync(FsEntry fsEntry, bool toggle)
+    {
+        if (toggle)
+            SelectedEntries.Add(fsEntry);
+        else
+            SelectedEntries.Remove(fsEntry);
+        
+        await InvokeAsync(StateHasChanged);
+    }
+}
