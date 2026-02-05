9d0260b345dd008fe90e34b75a3a25200b5551e2
ChiaraBm <chiara.bm04@gmail.com>
2026-02-05 07:30:33 +0100

    Implemented improved downloading for testing of chunked upload. Adjusted type of toast in file dropzone

---
+++ ShadcnBlazor.Test/wwwroot/fileManagerDemo.js
@@ -0,0 +1,30 @@
+﻿window.fileManagerDemo = {
+    download: async function (fileName, streamRef) {
+        // Get the response from the stream reference
+        const response = await streamRef.stream();
+        const reader = response.getReader();
+
+        // Show file picker to choose save location
+        const handle = await window.showSaveFilePicker({
+            suggestedName: fileName
+        });
+
+        // Create a writable stream and wait for it to be ready
+        const writableStream = await handle.createWritable();
+
+        // Function to pump data from the reader to the writable stream
+        const pump = async () => {
+            const { done, value } = await reader.read();
+            if (done) {
+                await writableStream.close();
+                console.log("done");
+                return;
+            }
+            await writableStream.write(value);
+            return pump(); // Call pump again for the next chunk
+        };
+
+        // Start pumping data
+        await pump();
+    }
+}
