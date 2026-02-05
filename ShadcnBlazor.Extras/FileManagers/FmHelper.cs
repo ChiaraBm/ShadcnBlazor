d7279c326374062250341cbfba3d7ad3511c2a6c
ChiaraBm <chiara.bm04@gmail.com>
2026-02-05 12:14:39 +0100

    Merged editor helper and file manager icon method to FmHelper ro cleanup duplicated code

--- ShadcnBlazor.Extras/FileManagers/FmHelper.cs
+++ ShadcnBlazor.Extras/FileManagers/FmHelper.cs
@@ -1,17 +1,92 @@
 ﻿using LucideBlazor;
 using Microsoft.AspNetCore.Components;
+using ShadcnBlazor.Extras.Editors;
 
 namespace ShadcnBlazor.Extras.FileManagers;
 
-public partial class FileManager
+internal static class FmHelper
 {
-    private static RenderFragment GetIcon(string fileName)
+    private static readonly Dictionary<string, EditorLanguage> AllowedExtensions = new()
+    {
+        // JavaScript/TypeScript
+        ["js"] = EditorLanguage.Javascript,
+        ["mjs"] = EditorLanguage.Javascript,
+        ["cjs"] = EditorLanguage.Javascript,
+        ["ts"] = EditorLanguage.Javascript,
+        ["jsx"] = EditorLanguage.Javascript,
+        ["tsx"] = EditorLanguage.Javascript,
+    
+        // HTML
+        ["html"] = EditorLanguage.Html,
+        ["htm"] = EditorLanguage.Html,
+    
+        // JSON
+        ["json"] = EditorLanguage.Json,
+        ["jsonc"] = EditorLanguage.Json,
+        ["json5"] = EditorLanguage.Json,
+    
+        // SQL
+        ["sql"] = EditorLanguage.Sql,
+    
+        // YAML
+        ["yml"] = EditorLanguage.Yaml,
+        ["yaml"] = EditorLanguage.Yaml,
+    
+        // PHP
+        ["php"] = EditorLanguage.Php,
+    
+        // XML
+        ["xml"] = EditorLanguage.Xml,
+    
+        // Python
+        ["py"] = EditorLanguage.Python,
+    
+        // CSS
+        ["css"] = EditorLanguage.Css,
+        ["scss"] = EditorLanguage.Css,
+        ["sass"] = EditorLanguage.Css,
+        ["less"] = EditorLanguage.Css,
+    
+        // Configuration files
+        ["txt"] = EditorLanguage.None,
+        ["md"] = EditorLanguage.None,
+        ["env"] = EditorLanguage.None,
+        ["properties"] = EditorLanguage.None,
+        ["conf"] = EditorLanguage.None,
+        ["cfg"] = EditorLanguage.None,
+        ["ini"] = EditorLanguage.None,
+        ["toml"] = EditorLanguage.None,
+        ["lock"] = EditorLanguage.None,
+        ["log"] = EditorLanguage.None,
+    
+        // Java/Kotlin (Minecraft plugins)
+        ["java"] = EditorLanguage.None,
+        ["kt"] = EditorLanguage.None,
+        ["kts"] = EditorLanguage.None,
+    
+        // Shell scripts
+        ["sh"] = EditorLanguage.None,
+        ["bash"] = EditorLanguage.None,
+        ["bat"] = EditorLanguage.None,
+        ["cmd"] = EditorLanguage.None,
+        ["ps1"] = EditorLanguage.None,
+    };
+    
+    internal static EditorLanguage GetLanguage(string extension)
+        => AllowedExtensions.GetValueOrDefault(extension);
+
+    internal static bool CheckExtension(string extension) => AllowedExtensions.ContainsKey(extension);
+
+    internal static string GetExtension(string fileName)
     {
         var parts = fileName.Split('.');
 
-        var extension = parts.Length == 1
-            ? parts[0].ToLowerInvariant()
-            : parts[parts.Length - 1].ToLowerInvariant();
+        return parts.Length == 1 ? parts[0] : parts[parts.Length - 1];
+    }
+    
+    internal static RenderFragment GetIcon(string fileName)
+    {
+        var extension = GetExtension(fileName).ToLowerInvariant();
 
         var type = extension switch
         {
